edf93343816c1a67700fb92ebd8eaffdc7ef7ee2
glebys33 <glebys.kiselev@gmail.com>
2022-03-25 00:56:39 +0300

    helper function to clear the chat preparation for the game

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from config import BOT_TOKEN
 import discord
 from discord.ext import commands
 from discord.utils import get
+import random
 
 
 bot = commands.Bot(command_prefix='!')
@@ -48,6 +49,44 @@ class Leading(commands.Cog):
         if voice and voice.is_connected():
             await voice.disconnect()
             await ctx.send(f'Бот отключился от: {channel}')
+
+    # вспомогательная функция для очистки чата (потом удалить)
+    @commands.command(name='clear')
+    async def clear(self, ctx, a=None):
+        if a is None:
+            await ctx.send('скока? в конце напиши')
+        else:
+            await ctx.channel.purge(limit=int(a))
+
+    # функция игры
+    @commands.command(name='start')
+    async def start_game(self, ctx):
+        # список игроков
+        players = ctx.message.author.voice.channel.members
+
+        if len(players) < 6:
+            await ctx.send('Вас слишком мало')
+            return
+
+            # выдаем роли
+        n_mafia = round(len(players) / 3.5)
+        mafia = {i: discord.PermissionOverwrite for i in random.sample(players, n_mafia)}
+        for i in mafia:
+            players.pop(players.index(i))
+        sheriff = random.choice(players)
+        players.pop(players.index(sheriff))
+        doctor = random.choice(players)
+        players.pop(players.index(doctor))
+
+        # создаем новую категория, голосоой чат и чат для мафии
+        guild = ctx.guild
+        game = await guild.create_category('Мафия')
+        voice = await guild.create_voice_channel('Игра', category=game)
+        mafia_text = await guild.create_text_channel('Мафия', category=game, overwrites=mafia)
+
+        # перемещанм всех игроков в игровой войс канал
+        for i in players:
+            await i.move_to(voice)
 
 
 bot.add_cog(Leading(bot))
