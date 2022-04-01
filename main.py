b6513b988cbdc453bc22f11b50122531c7867160
glebys33 <glebys.kiselev@gmail.com>
2022-04-01 21:50:02 +0300

    preparation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from config import BOT_TOKEN
 import discord
+from discord import PermissionOverwrite
 from discord.ext import commands
 from discord.utils import get
 import random
@@ -15,10 +16,8 @@ async def on_ready():
 
 
 class Leading(commands.Cog):
-
     def __init__(self, bot):
         self.bot = bot
-        self.voice = None
 
     # личное сообщение для автора
     @commands.command(name='lsa')
@@ -60,33 +59,56 @@ class Leading(commands.Cog):
 
     # функция игры
     @commands.command(name='start')
-    async def start_game(self, ctx):
+    async def start_game(self, ctx, doc='есть', com='есть'):
+        if ((com == 'есть') or (com == 'нету')) and ((doc == 'есть') or (doc == 'нету')):
+            await ctx.send('Некоректные настройки')
+            return
+
         # список игроков
         players = ctx.message.author.voice.channel.members
+        access = {'manage_channels': True, 'manage_roles': True, 'read_messages': True, 'connect': True, 'speak': True}
+        ban = {'manage_channels': False, 'manage_roles': False, 'read_messages': False, 'connect': False, 'speak': False}
+        for i in players:
+            if 'mafia#7771' == str(i):
+                players.pop(players.index(i))
 
         if len(players) < 6:
             await ctx.send('Вас слишком мало')
             return
 
-            # выдаем роли
+        # выдаем роли
         n_mafia = round(len(players) / 3.5)
-        mafia = {i: discord.PermissionOverwrite for i in random.sample(players, n_mafia)}
+        mafia = {i: discord.PermissionOverwrite(**access) for i in random.sample(players, n_mafia)}
         for i in mafia:
             players.pop(players.index(i))
-        sheriff = random.choice(players)
-        players.pop(players.index(sheriff))
-        doctor = random.choice(players)
-        players.pop(players.index(doctor))
+        if com == 'есть':
+            sheriff = random.choice(players)
+            players.pop(players.index(sheriff))
+        if doc == 'есть':
+            doctor = random.choice(players)
+            players.pop(players.index(doctor))
 
-        # создаем новую категория, голосоой чат и чат для мафии
+        # создаем новые приватные категорию, голосоой чат и чаты для мафии, комиссар и доктора
         guild = ctx.guild
-        game = await guild.create_category('Мафия')
+        game = await guild.create_category('Мафия', overwrites={i: PermissionOverwrite(**access) for i in ctx.message.author.voice.channel.members})
+        await game.set_permissions(guild.default_role, overwrite=PermissionOverwrite(**ban))
         voice = await guild.create_voice_channel('Игра', category=game)
+        await voice.set_permissions(guild.default_role, overwrite=PermissionOverwrite(**ban))
+        mafia_text = await guild.create_text_channel('Игра', category=game)
+        await mafia_text.set_permissions(guild.default_role, overwrite=PermissionOverwrite(**ban))
         mafia_text = await guild.create_text_channel('Мафия', category=game, overwrites=mafia)
+        await mafia_text.set_permissions(guild.default_role, overwrite=PermissionOverwrite(**ban))
+        if com == 'есть':
+            sheriff_text = await guild.create_text_channel('Шериф', category=game, overwrites={sheriff: PermissionOverwrite(**access)})
+            await sheriff_text.set_permissions(guild.default_role, overwrite=PermissionOverwrite(**ban))
+        if doc == 'есть':
+            doctor_text = await guild.create_text_channel('Доктор', category=game, overwrites={doctor: PermissionOverwrite(**access)})
+            await doctor_text.set_permissions(guild.default_role, overwrite=PermissionOverwrite(**ban))
 
         # перемещанм всех игроков в игровой войс канал
         for i in players:
             await i.move_to(voice)
+        await get(bot.voice_clients).move_to(voice)
 
 
 bot.add_cog(Leading(bot))
